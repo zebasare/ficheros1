4f350819f5e8f357f0ae196c7298c54369556db5
zebasare <zebasare@yahoo.com>
2019-12-19 18:45:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from io import open
+
+texto="Vamos Vamos Penarol ,\n Hoy te vinimos a alentar,\n Para ser campeon , \n Hoy hay que ganar"
+
+
+fichero=open("penarol.txt","w")
+fichero.write(texto) 
+fichero.close()
+
+
