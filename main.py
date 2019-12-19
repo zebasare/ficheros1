e24e5ca3cdd59dcb93d6aa1c4308e4880a5bda09
zebasare <zebasare@yahoo.com>
2019-12-19 19:56:09 +0000

    Append text

--- main.py
+++ main.py
@@ -2,32 +2,52 @@ from io import open
 
 texto="Vamos Vamos Penarol ,\n Hoy te vinimos a alentar,\n Para ser campeon , \n Hoy hay que ganar"
 
-
 fichero=open("penarol.txt","w")
 fichero.write(texto) 
 fichero.close()
-del(fichero)
-del(texto)
+
+##################################################################################################
+
 fichero=open("penarol.txt","r")
 #el metodo read() lee todas las lineas del fichero 
 texto=fichero.read()
 #Siempre hay que cerrar el fichero despues de haberlo utilizado
 fichero.close()
+print(texto) 
 
-print(texto)
-
-del(fichero)
-del(texto)
+##################################################################################################
 
 fichero=open("penarol.txt","r")
 #readlines() lee cada linea y la guarda como elemento de una lista
 texto=fichero.readlines()
-
+fichero.close()
 
 print("")
 
 for idx,line in enumerate(texto): 
   print("Linea ",idx+1,"=> ",line)
+
+##################################################################################################
+
+
+fichero=open("penarol.txt","a")
+
+fichero.write("\n Carbone , Carbone, Carbone.....")
+fichero.close()
+fichero=open("penarol.txt","r")
+lineas=fichero.readlines()
+fichero.close()
+
+print(lineas)
+
+print("")
+
+for idx,lin in enumerate(lineas): 
+  print(idx+1,lin)
+
+
+
+
 
 
 
